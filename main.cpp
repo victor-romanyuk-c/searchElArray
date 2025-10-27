9be4f75a3f06d85a9416a7e8478a14020a1e92aa
User <User@LAPTOP-8UIAMBOL>
2025-10-27 11:21:51 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int searchArrEl(int*, int, int);
+
+int main()
+{
+	int numbers[] = { 5, 8, 4, 1, 6, 4, 85, 9 };
+	int size = sizeof(numbers) / sizeof(numbers[0]);
+	int index = 0, myNum = 0;
+
+	std::cout << "Enter a number to search for: \n";
+	std::cin >> myNum;
+
+	index = searchArrEl(numbers, size, myNum);
+
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
+	fflush(stdin);
+
+	if (index != -1)
+	{
+		std::cout << "Element " << myNum << ", is at index "\
+			<< index << '\n';
+	}
+	else
+	{
+		std::cout << "Number " << myNum << " is not in the array.\n";
+	}
+
+	return 0;
+}
+
+int searchArrEl(int* Array, int size, int element)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (Array[i] == element)
+		{
+			std::cout << "Element found at index: " << i << std::endl;
+			return i;
+		}
+	}
+	return 0;
+}
